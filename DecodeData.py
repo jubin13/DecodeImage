5bdccc6ca4e516380327ed69074139b9ad8571f4
Nate Hopkins <smartsystemdesign@gmail.com>
2016-08-26 13:18:36 -0600

    Now reading IR camera data and saving an image correctly.  GenerateData generates a mock camera frame with random pixel values that can be used with DecodeData (replace file name on line 13 with data.csv).

--- DecodeData.py
+++ DecodeData.py
@@ -6,24 +6,24 @@ from PIL import Image
 import numpy as np
 import csv
 
-w, h = 256, 256
+w, h = 59, 79
 data = np.zeros((h, w), dtype=np.uint8)
 # data = np.zeros((h, w), np.int)
 
-with open('data.csv', 'r') as f:
-    #my_list = [[int(x) for x in rec] for rec in csv.reader(f, dialect='excel', delimiter=',')]
-
-    for x, row in enumerate(csv.reader(f, dialect='excel', delimiter=',')):
+with open('Flir_Image_of_me.csv', 'r') as f:
+    for x, row in enumerate(csv.reader(f, dialect='excel', delimiter='\t')):
         row.pop(w) # remove \n
         for y, column in enumerate(row):
             try:
                 data[x, y] = int(column)
             except ValueError:
                 pass
+            except IndexError:
+                print("error")
 
     print(data)
 
 
 img = Image.fromarray(data, 'L')
-img.save('my.png')
+img.save('image.png')
 img.show()
--- GenerateData.py
+++ GenerateData.py
@@ -1,11 +1,11 @@
 import random, pathlib
 # from sys import random
 f = open('data.csv', 'w')
-w, h = 256, 256
+w, h = 59, 79
 
 for y in range(h):
     for x in range(w):
-        f.write(str(random.randrange(0, 255)) + ',')
+        f.write(str(random.randrange(0, 255)) + '\t')
     f.write('\n')
 
 f.close()
